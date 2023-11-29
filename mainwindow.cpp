a548825c979f9b66ff00937fa4f5be3f2c106fe5
DJTiesto <tiesto.tjolle@student.kuleuven.be>
2023-11-29 14:36:35 +0100

    Update of health and energy bars

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -254,22 +254,34 @@ void MainWindow::drawProtagonist() {
 }
 
 void MainWindow::drawBars() {
-    // Add visualization for protagonist health bar
-    int healthBarWidth = tileSize * 2; // You can adjust the width as needed
-    int healthBarHeight = tileSize / 4; // You can adjust the height as needed
-    QRect healthBarRect(protagonist.getXPos() * tileSize, protagonist.getYPos() * tileSize - tileSize / 2, healthBarWidth, healthBarHeight);
+    // Define the position above the map for the bars
+    int barX = 0; // X-coordinate
+    int barY = -tileSize * 2; // Y-coordinate, adjust as needed
+
+    // Calculate the remaining health as a percentage
     double healthRatio = static_cast<double>(protagonist.getHealth()) / static_cast<double>(maxEH);
+
+    // Calculate the width of the health bar based on the remaining health
+    int healthBarWidth = static_cast<int>(myWorld.getCols() * tileSize * healthRatio);
+
+    // Clear the old health bar rectangle
+    QRect oldHealthBarRect(barX, barY, myWorld.getCols() * tileSize, tileSize / 2); // Full width of the map
+    scene->addRect(oldHealthBarRect, QPen(Qt::white), QBrush(Qt::white)); // Clear the old health bar
+
+    // Add visualization for the sliding health bar
+    QRect healthBarRect(barX, barY, healthBarWidth, tileSize / 2); // You can adjust the height as needed
     QColor healthBarColor = QColor::fromRgbF(1.0 - healthRatio, healthRatio, 0.0); // Red to green gradient
     scene->addRect(healthBarRect, QPen(Qt::black), QBrush(healthBarColor));
 
-    // Add visualization for protagonist energy bar
-    int energyBarWidth = tileSize * 2; // You can adjust the width as needed
-    int energyBarHeight = tileSize / 4; // You can adjust the height as needed
-    QRect energyBarRect(protagonist.getXPos() * tileSize, protagonist.getYPos() * tileSize - tileSize / 2 - energyBarHeight, energyBarWidth, energyBarHeight);
+    // Add visualization for protagonist energy bar (similar to previous implementation)
+    int energyBarWidth = myWorld.getCols() * tileSize; // Full width of the map
+    int energyBarHeight = tileSize / 2; // You can adjust the height as needed
+    QRect energyBarRect(barX, barY - energyBarHeight, energyBarWidth, energyBarHeight);
     double energyRatio = static_cast<double>(protagonist.getEnergy()) / static_cast<double>(maxEH);
     QColor energyBarColor = QColor::fromRgbF(0.0, 0.0, 1.0 - energyRatio); // Blue to black gradient
     scene->addRect(energyBarRect, QPen(Qt::black), QBrush(energyBarColor));
 }
+
 
 void MainWindow::keyPressEvent(QKeyEvent *event) {
   int newX = protagonist.getXPos();
@@ -344,6 +356,7 @@ void MainWindow::attackEnemy()
                 enemy->setDefeated(true);
                 scene->addRect(enemy->getXPos() * tileSize, enemy->getYPos() * tileSize, tileSize, tileSize, QPen(Qt::black), QBrush(QColorConstants::Svg::purple));
                 drawProtagonist();
+                drawBars();
             }
             else
             {
@@ -380,6 +393,7 @@ void MainWindow::useHealthpack()
                 }
                 scene->addRect(pack->getXPos() * tileSize, pack->getYPos() * tileSize, tileSize, tileSize, QPen(Qt::black), QBrush(QColorConstants::Svg::purple));
                 drawProtagonist();
+                drawBars();
             }
             // Exit the function since the attack has been resolved
             return;
