de061132b66973f31cbd36ec42e99027a858eb00
DJTiesto <tiesto.tjolle@student.kuleuven.be>
2023-12-28 20:46:13 +0100

    Fixed width and height for health and energy bars

--- controller/windowcontroller.cpp
+++ controller/windowcontroller.cpp
@@ -55,23 +55,23 @@ void WindowController::drawBars(){
     double healthRatio = static_cast<double>(worldController->getProtagonist()->getHealth()) / static_cast<double>(worldController->getMaxEH());
 
     // Calculate the width of the health bar based on the remaining health
-    int healthBarWidth = static_cast<int>(worldController->getCols() * tileSize * healthRatio);
+    int healthBarWidth = static_cast<int>(40 * tileSize * healthRatio);
 
     // Clear the old health bar rectangle
-    QRect oldHealthBarRect(barX, barY, worldController->getCols() * tileSize, tileSize); // Full width of the map
+    QRect oldHealthBarRect(barX, barY, 40 * tileSize, 20); // Full width of the map
     scene->addRect(oldHealthBarRect, QPen(Qt::white), QBrush(Qt::white)); // Clear the old health bar
 
     // Add visualization for the sliding health bar
-    QRect healthBarRect(barX, barY, healthBarWidth, tileSize); // You can adjust the height as needed
+    QRect healthBarRect(barX, barY, healthBarWidth, 20); // You can adjust the height as needed
     QColor healthBarColor = QColor::fromRgbF(1.0 - healthRatio, healthRatio, 0.0); // Red to green gradient
     scene->addRect(healthBarRect, QPen(Qt::black), QBrush(healthBarColor));
 
     // Add visualization for protagonist energy bar (similar to previous implementation)
     double energyRatio = static_cast<double>(worldController->getProtagonist()->getEnergy()) / static_cast<double>(worldController->getMaxEH());
-    int energyBarWidth = worldController->getCols() * tileSize * energyRatio; // Full width of the map
-    int energyBarHeight = tileSize; // You can adjust the height as needed
+    int energyBarWidth = 40 * tileSize * energyRatio; // Full width of the map
+    int energyBarHeight = 20; // You can adjust the height as needed
 
-    QRect oldEnergyBarRect(barX, barY - energyBarHeight, worldController->getCols() * tileSize, energyBarHeight); // Full width of the map
+    QRect oldEnergyBarRect(barX, barY - energyBarHeight, 40 * tileSize, 20); // Full width of the map
     scene->addRect(oldEnergyBarRect, QPen(Qt::white), QBrush(Qt::white)); // Clear the old energy bar
 
     QRect energyBarRect(barX, barY - energyBarHeight, energyBarWidth, energyBarHeight);
