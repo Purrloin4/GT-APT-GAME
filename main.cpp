9e6680c6ec89ce95efc01858ce2126787da6337d
BobbieBambino <bob.heylen@student.kuleuven.be>
2024-01-13 22:34:47 +0100

    Full screen game startup

--- main.cpp
+++ main.cpp
@@ -8,8 +8,8 @@ int main(int argc, char *argv[])
     QLoggingCategory pathfinderCategory("main", QtDebugMsg);
     QApplication a(argc, argv);
     QLoggingCategory::setFilterRules("pathfinderCategory.debug=true");
-//    MainWindow w; // ctor - full screen
-    MainWindow w(916, 600); // ctor - specified dimensions
+    MainWindow w; // ctor - full screen
+//    MainWindow w(916, 600); // ctor - specified dimensions
     //qCDebug(main) << w.getView().scene()->items().count();
     w.show();
     return a.exec();
